9640b17a9cc86055b3376d15fd10def1f4f60ab1
Raykkoner <raykkonerd@gmail.com>
2024-06-21 17:05:36 -0300

    fix: correções menores em modais da seção Fatores de risco da tela Hipertensão

--- src/screens/Home/data/HypertensionCardsData.js
+++ src/screens/Home/data/HypertensionCardsData.js
@@ -13,7 +13,7 @@ export const cardsList = [
     cardTitle: "Genética",
     cardDescription: "Pode influenciar os níveis de pressão arterial entre...",
     cardImg: dnaImage,
-    modalTitle: "Hipertensão",
+    modalTitle: "Fatores de risco",
     modalSubtitle: "Genética",
     modalDescription: "Pode influenciar os níveis de pressão arterial entre 30-50%. No entanto, devido à ampla diversidade de genes, às variantes genéticas estudadas até o momento e aos grupos étnicos diferentes em nosso país, não foram identificados dados uniformes com relação a tal fator."
   },
@@ -21,7 +21,7 @@ export const cardsList = [
     cardTitle: "Idade",
     cardDescription: "Com o envelhecimento, a Pressão...",
     cardImg: idadeImage,
-    modalTitle: "Hipertensão",
+    modalTitle: "Fatores de risco",
     modalSubtitle: "Idade",
     modalDescription: "Com o envelhecimento, a Pressão Arterial Sistólica (PAS) torna-se um problema mais importante, resultante do enrijecimento progressivo e da perda de complacência das grandes artérias. Por isso que com o tempo, a pessoa tende a ter a pressão arterial mais alta."
   },
@@ -29,15 +29,15 @@ export const cardsList = [
     cardTitle: "Sexo",
     cardDescription: "Nos mais jovens, a pressão arterial é...",
     cardImg: sexoImage,
-    modalTitle: "Hipertensão",
+    modalTitle: "Fatores de risco",
     modalSubtitle: "Sexo",
-    modalDescription: "Nos mais jovens, a pressão arterial é mais elevada entre os homens, mas não quer dizer que as mulheres estao isentas. Em mulheres com 60 anos, a pressão arterial costuma ser mais elevada e a prevalência de hipertensão (pressão alta) maior. Em ambos os sexos, a frequência de hipertensão aumenta com a idade, alcançando 61,5% dos homens e 68,0% das mulheres na faixa etária de 65 anos ou mais."
+    modalDescription: "Nos mais jovens, a pressão arterial é mais elevada entre os homens, mas não quer dizer que as mulheres estão isentas. Em mulheres com 60 anos, a pressão arterial costuma ser mais elevada e a prevalência de hipertensão (pressão alta) maior. Em ambos os sexos, a frequência de hipertensão aumenta com a idade, alcançando 61,5% dos homens e 68,0% das mulheres na faixa etária de 65 anos ou mais."
   },
   {
     cardTitle: "Raça",
     cardDescription: "A etnia negra tem um risco...",
     cardImg: racaImage,
-    modalTitle: "Hipertensão",
+    modalTitle: "Fatores de risco",
     modalSubtitle: "Raça",
     modalDescription: "A etnia negra tem um risco maior de desenvolver hipertensão."
   },
@@ -45,7 +45,7 @@ export const cardsList = [
     cardTitle: "Obesidade",
     cardDescription: "O ganho de apenas 20% sobre o peso...",
     cardImg: obesidadeImage,
-    modalTitle: "Hipertensão",
+    modalTitle: "Fatores de risco",
     modalSubtitle: "Obesidade",
     modalDescription: "O ganho de apenas 20% sobre o peso normal pode aumentar em até oito vezes o risco de hipertensão arterial. "
   },
@@ -53,31 +53,31 @@ export const cardsList = [
     cardTitle: "Ingestão de sódio",
     cardDescription: "A ingestão elevada de sódio pode aumentar...",
     cardImg: sodioImage,
-    modalTitle: "Hipertensão",
+    modalTitle: "Fatores de risco",
     modalSubtitle: "Ingestão de sódio",
-    modalDescription: "A ingestão elevada de sódio pode aumentar a pressão arterial. A literatura científica mostra que a ingestão de sódio está associada à Doenças Cardiovasculares e Acidente Vascular Encefalico (AVE) , quando a ingestão média é superior a 2 g de sódio, o equivalente a 5 g de sal de cozinha."
+    modalDescription: "A ingestão elevada de sódio pode aumentar a pressão arterial. A literatura científica mostra que a ingestão de sódio está associada a Doenças Cardiovasculares e Acidente Vascular Encefálico (AVE), quando a ingestão média é superior a 2g de sódio, o equivalente a 5g de sal de cozinha."
   },
   {
     cardTitle: "Álcool",
     cardDescription: "Aquelas pessoas que ingerem...",
     cardImg: alcoolImage,
-    modalTitle: "Hipertensão",
+    modalTitle: "Fatores de risco",
     modalSubtitle: "Álcool",
-    modalDescription: "Aquelas pessoas que ingerem seis ou mais doses ao dia de álcool tem mais risco de desenvolver pressão alta, o equivalente a 30 g de álcool/dia = 1 garrafa de cerveja (5% de álcool, 600 mL); = 2 taças de vinho (12% de álcool, 250 mL); = 1 dose (42% de álcool, 60 mL) de destilados (uísque, vodca, aguardente). Esse limite deve ser reduzido à metade para homens de baixo peso e mulheres."
+    modalDescription: "Aquelas pessoas que ingerem seis ou mais doses ao dia de álcool têm mais risco de desenvolver pressão alta, o equivalente a 30g de álcool/dia = 1 garrafa de cerveja (5% de álcool, 600 ml); = 2 taças de vinho (12% de álcool, 250 ml); = 1 dose (42% de álcool, 60 ml) de destilados (uísque, vodca, aguardente). Esse limite deve ser reduzido à metade para homens de baixo peso e mulheres."
   },
   {
     cardTitle: "Fatores socio-econômicos",
     cardDescription: "Entre os fatores, podemos...",
     cardImg: socioeconomicolImage,
-    modalTitle: "Hipertensão",
+    modalTitle: "Fatores de risco",
     modalSubtitle: "Fatores socioeconômicos",
-    modalDescription: "Entre os fatores socioeconômicos, podemos destacar menor escolaridade e condições de habitação inadequadas, além da baixa renda familiar"
+    modalDescription: "Entre os fatores socioeconômicos, podemos destacar menor escolaridade e condições de habitação inadequadas, além da baixa renda familiar."
   },
   {
     cardTitle: "Sedentarismo",
     cardDescription: "O indivíduo que não pratica atividade...",
     cardImg: sedentarismo,
-    modalTitle: "Hipertensão",
+    modalTitle: "Fatores de risco",
     modalSubtitle: "Sedentarismo",
     modalDescription: "O indivíduo que não pratica atividade física tem o risco de desenvolver hipertensão. Estudos mostraram que, no mundo inteiro, a falta de atividade física era mais presente nas mulheres (31,7%) do que nos homens (23,4%). "
   }
